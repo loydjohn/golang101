fabae1ab4bc60b6c6abfadb9aed0024ee0386997
Loyd <loydjohn@gmail.com>
2018-07-08 01:06:40 +0800

    Add files via upload

---
+++ switch.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	i := 2
+	fmt.Println("Write ", i, "as")
+	switch i {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	}
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+	whatAmI := func() {
+
+	}
+}
